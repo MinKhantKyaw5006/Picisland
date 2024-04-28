0ac5e0cb1bd0c7741b4e3ab40552e4d4698644ee
MinKhantKyaw <minkhant050603@gmail.com>
2024-04-28 23:55:53 +0700

    Update FetchUser.js

--- picisland_frontend/src/utils/FetchUser.js
+++ picisland_frontend/src/utils/FetchUser.js
@@ -1,5 +1,5 @@
 export const FetchUser = () => {
     const userInfo = localStorage.getItem('user') ? JSON.parse(localStorage.getItem('user')) : null;
-    //console.log('User Info:', userInfo); // Add this line for debugging
+    console.log('User Info:', userInfo); // Add this line for debugging
     return userInfo;
 }
